de825307181d8b83406280ab6bc9b65d6b9c8f3c
cynthia Amanat <cynthiasusana.ary@gmail.com>
2022-10-10 21:19:56 +0200

    added client

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ if (process.env.NODE_ENV === "production") {
   // set static folder
   app.use(express.static("cilent/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "cilent", "build", "index.html"));
   });
 }
 
